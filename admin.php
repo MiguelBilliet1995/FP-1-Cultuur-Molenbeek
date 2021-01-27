b09878824011eefd3dfb5572d318603278a3fe10
MiguelBilliet1995 <miguel.billiet@student.ehb.be>
2021-01-27 14:34:46 +0100

    admin panel

---
+++ admin.php
@@ -0,0 +1,26 @@
+<?php
+
+  include_once("parser/evenementenDB.php");
+  include_once("parser/newsDB.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Admin panel</title>
+
+  <link rel="stylesheet" href="https://use.typekit.net/yvs4yrr.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+  <link rel="stylesheet" href="styles/admin-panel.css">
+  <link rel="stylesheet" href="styles/style-fonts.css">
+  <script src="scripts/script.js"></script>
+
+</head>
+<body>
+  
+</body>
+</html>
