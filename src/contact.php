7c9ccb4bce4dd895652d89c8765060a2ce205ed1
Gill Mertens <gill.mertens@student.ehb.be>
2021-01-28 15:10:27 +0100

    footer include

--- src/contact.php
+++ src/contact.php
@@ -72,46 +72,7 @@
       </div>
     </div>
   </main>
-
-  <footer>
-    <div class="container">
-      <div class="row">
-
-        <div class="col-sm-4">
-          <ul class="footer-navigation">
-            <li><a href="index.html">Home</a></li>
-            <li><a href="evenementen.html">Kalender</a></li>
-            <li><a href="news.html">Nieuws</a></li>
-            <li><a href="about.html">Over ons</a></li>
-            <li><a href="contact.html" class="active">Contact</a></li>
-          </ul>
-        </div>
-
-        <div class="col-sm-4">
-          <span><b>Contactinfo</b><br />
-            info@familiemolenbeek.be<br />
-            +32 468 10 99 14 <br>
-            <div class="sociallinks">
-              <a href="https://www.instagram.com/cultuur_in_molenbeek/" target="_blank"><img
-                  src="images/icons/instagram-logo.svg" alt=""></a>
-              <a href="https://www.facebook.com/Cultuur-in-Molenbeek-770192383096967" target="_blank"><img
-                  src="images/icons/facebook-brands.svg" alt=""></a>
-            </div>
-          </span>
-        </div>
-
-        <div class="col-sm-4">
-          </ul>
-          <div class="year">
-            <li>2020-2021</li>
-            <li> &copy; Familie Molenbeek</li>
-          </div>
-          </ul>
-        </div>
-
-      </div>
-    </div>
-  </footer>
+  <?php include 'include/footer.php'; ?>
 </body>
 
 </html>
--- src/news.php
+++ src/news.php
@@ -128,52 +128,7 @@
     </div>
     <!-- end article -->
   </div>
-
-  <footer>
-    <div class="container">
-      <div class="row">
-
-        <div class="col-sm-4">
-          <ul class="footer-navigation">
-            <li><a href="index.html">Home</a></li>
-            <li><a href="evenementen.html">Kalender</a></li>
-            <li><a href="news.html" class="active">Nieuws</a></li>
-            <li><a href="about.html">Over ons</a></li>
-            <li><a href="contact.html">Contact</a></li>
-          </ul>
-        </div>
-
-        <div class="col-md-4">
-          <span><b>Contactinfo</b><br />
-            info@familiemolenbeek.be<br />
-            +32 468 10 99 14 <br>
-            <div class="sociallinks">
-              <a href="https://www.instagram.com/cultuur_in_molenbeek/" target="_blank"><img
-                  src="images/icons/instagram-logo.svg" alt=""></a>
-              <a href="https://www.facebook.com/Cultuur-in-Molenbeek-770192383096967" target="_blank"><img
-                  src="images/icons/facebook-brands.svg" alt=""></a>
-            </div>
-          </span>
-        </div>
-
-        <div class="col-sm-4">
-          <div class="footerlogos"><img src="images/logos/bib-logo.svg" alt=""><img
-              src="images/logos/cultuurinmolebeek.svg" alt=""></div>
-          <div class="footerlogos"><img src="images/logos/vk-logo.svg" alt=""><img src="images/logos/molenbeek-logo.svg"
-              alt=""></div>
-          </ul>
-          <div class="year">
-            <li>2020-2021</li>
-            <li> &copy; Familie Molenbeek</li>
-          </div>
-          </ul>
-        </div>
-
-      </div>
-    </div>
-  </footer>
-
-
+  <?php include 'include/footer.php'; ?>
   <script defer src="./scripts/all.js"></script>
   <script src="scripts/script.js"></script>
 </body>
