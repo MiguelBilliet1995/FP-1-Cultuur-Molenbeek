9b2749f94ed03ca9f9d234a96e67d076747afbe4
Gill Mertens <gill.mertens@student.ehb.be>
2021-02-02 11:41:23 +0100

    sort Event listner

--- src/scripts/sort.js
+++ src/scripts/sort.js
@@ -2,3 +2,10 @@ document.querySelector('.filter-listener').addEventListener('change', (e) => {
     console.log(e.target.value);
     window.location.href = `evenementen.php?filter=type&filterdata=${e.target.value}`;
 });
+
+document.querySelector('.sort-listener').addEventListener('change', (e) => {
+    let id = e.target.value.split("%");
+    console.log(id);
+    console.log(e.target.value);
+    window.location.href = `evenementen.php?sort=${id[1]}&sortdirection=${id[2]}`;
+});
