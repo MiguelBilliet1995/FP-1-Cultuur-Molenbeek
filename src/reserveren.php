db6ae85a2df23020012e5dc83e16fa537ddc9e9a
MiguelBilliet1995 <miguel.billiet@student.ehb.be>
2021-02-04 23:17:02 +0100

    reserveren update

--- src/reserveren.php
+++ src/reserveren.php
@@ -46,12 +46,25 @@
         <div class="col-sm-12 col-lg-6 event-id-info">
           <img src='data/images/evenementen/<?php echo $evenement->foto();?>'>
           <div class="textbubblecontainer">
-              <span class="language">nl:</span>
-              <img src="images/icons/speechbubblefull.svg" alt="">
-              <img src="images/icons/speechbubblefull.svg" alt="">
-              <img src="images/icons/speechbubbleempty.svg" alt="">
-              <img src="images/icons/speechbubbleempty.svg" alt="">
-              <img src="images/icons/speechbubbleempty.svg" alt="">
+              <img src="images/icons/speechbubblefullnl.svg" alt="">
+
+              <?php
+                $maxAantalIconen = 4;
+                $totaalIconen = $evenement->taalNiveau();
+                $indexIconen = 0;
+                for ($indexIconen=0; $indexIconen < $totaalIconen; $indexIconen++) { 
+              ?>
+                <img src="images/icons/speechbubblefull.svg" alt="">
+              <?php
+                }
+
+                for (null; $indexIconen < $maxAantalIconen; $indexIconen++) { 
+              ?>
+                <img src="images/icons/speechbubbleempty.svg" alt="">
+              <?php
+                }
+              ?>
+
             </div>
           <p><?php echo $evenement->beschrijving();?></p>
           <ul class="event-list">
